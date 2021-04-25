50dc83f771c1eac1e4f6ed627026e62d566e49ca
pritamSarkar123 <pritamsarkar84208220@gmail.com>
2021-04-25 12:43:05 +0530

    ok to pass static inner class obj to other class like PQ

--- greedy/ZeroOneKnapSackGreedy.java
+++ greedy/ZeroOneKnapSackGreedy.java
@@ -3,24 +3,25 @@ package greedy;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-class Item{
-	private int weight;
-	private int profit;
-	private double profitPerWeight;
-	public Item(int weight,int profit,double profitPerWeight) {
-		this.weight=weight;this.profit=profit;this.profitPerWeight=profitPerWeight;
-	}
-	public int getWeight() {
-		return weight;
-	}
-	public int getProfit() {
-		return profit;
-	}
-	public double getProfitPerWeight() {
-		return profitPerWeight;
-	}
-}
+
 public class ZeroOneKnapSackGreedy {
+	private static class Item{
+		private int weight;
+		private int profit;
+		private double profitPerWeight;
+		public Item(int weight,int profit,double profitPerWeight) {
+			this.weight=weight;this.profit=profit;this.profitPerWeight=profitPerWeight;
+		}
+		public int getWeight() {
+			return weight;
+		}
+		public int getProfit() {
+			return profit;
+		}
+		public double getProfitPerWeight() {
+			return profitPerWeight;
+		}
+	}
 	public static void main(String[] args) {
 		int []weights=new int[] {2,3,5,7,1,4,1};
 		int []profits=new int[] {10,5,15,7,6,18,3};
@@ -30,7 +31,7 @@ public class ZeroOneKnapSackGreedy {
 	public static void zeroOne(int []weights,int []profits,int bagSize) {
 		PriorityQueue<Item> pq=new PriorityQueue<>(Comparator.comparing(Item::getProfitPerWeight).reversed());
 		for(int i=0;i<weights.length;i++) {
-			pq.add(new Item(weights[i],profits[i],(double)profits[i]/(double)weights[i]));
+			pq.add(new ZeroOneKnapSackGreedy.Item(weights[i],profits[i],(double)profits[i]/(double)weights[i]));
 		}
 		int maxWeight=0;
 		double maxProfit=0;
